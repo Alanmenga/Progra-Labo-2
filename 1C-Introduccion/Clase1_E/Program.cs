a599a32bfa5ba49b8abd35b9b7c78f319ca684f7
Alanmenga <alan.gabriel.mengassini@gmail.com>
2023-09-04 19:09:15 -0300

    clase 2

---
+++ 1C-Introduccion/Clase1_E/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Clase1_E
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola soy alan mengassini");
+        }
+    }
+}
---
+++ 2C-ClasesMetodos/Validador/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Validador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantidadNumeros = 10;
+            int minimo = int.MaxValue;
+            int maximo = int.MinValue;
+            int suma = 0;
+
+            for (int i = 0; i < cantidadNumeros; i++)
+            {
+                Console.WriteLine($"Ingrese el numero {i + 1}: ");
+                int numero;
+
+                while (!int.TryParse(Console.ReadLine(), out numero))
+                {
+                    Console.WriteLine("Entrada invalida. Ingrese un numero valido.");
+                }
+
+                while (!Validador.Validar(numero, -100, 100))
+                {
+                    Console.WriteLine("Entrada invalida. Ingrese un numero valido.");
+                    Console.WriteLine("El numero debe estar en el rango de -100 a 100.");
+                    Console.WriteLine($"Ingrese el numero {i + 1} nuevamente: ");
+                    numero = int.Parse(Console.ReadLine());
+                }
+
+
+                int.TryParse(Console.ReadLine(), out numero);
+
+                minimo = Math.Min(minimo, numero);
+                maximo = Math.Max(maximo, numero);
+                suma += numero;
+            }
+
+            double promedio = (double)suma / cantidadNumeros;
+
+            Console.WriteLine($"Valor minimo: {minimo}");
+            Console.WriteLine($"Valor maximo: {maximo}");
+            Console.WriteLine($"Promedio : {promedio}");
+        }
+    }
+}
