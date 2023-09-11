855e4e14f42d4e5e71e95d62557d7d92198fc430
Alanmenga <alan.gabriel.mengassini@gmail.com>
2023-09-11 19:55:18 -0300

    mas clases y ejercicios

---
+++ 2C-POO/Ejercicios/E1-Prestamo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace E1_Prestamo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ 2C-POO/Ejercicios/Lib-Prestamo/Cuenta.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace Lib_Prestamo
+{
+    public class Cuenta
+    {
+        private string titular;
+        private int cantidad;
+
+        public Cuenta( string titular, int cantidad)
+        {
+            this.titular = titular;
+            this.cantidad = cantidad;
+        }
+
+        public string GetTitular() 
+        { 
+            return this.titular;
+        }
+
+        public int GetCantidad()
+        {
+            return this.cantidad;
+        }
+
+        public static string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine
+        }
+    }
+}
